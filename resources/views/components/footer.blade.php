63114c3ba2c211dbda0803a73a857d167ac608e1
Peter Askey <aspe@summacollege.nl>
2025-09-24 16:27:41 +0200

    initial commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="bg-dim mt-auto max-w-screen">
+    <figure class="w-screen">
+        <svg class="w-10 h-10 mx-auto mb-3 text-success" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+            <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+        </svg>
+        <blockquote>
+            <p class="text-2xl italic font-medium text-center">"Silence current side project, a new side project is talking."</p>
+        </blockquote>
+        <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
+            <img class="w-6 h-6 rounded-full" src="https://avatars.githubusercontent.com/u/94826098?v=4" alt="profile picture">
+            <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-fg">
+                <cite class="pe-3 font-medium  text-grey-1">Peter Askey</cite>
+                <cite class="ps-3 text-sm text=grey-2">Eindbaas @ Summa</cite>
+            </div>
+        </figcaption>
+    </figure>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<header class="bg-bg-dim fixed top-0 left-0 right-0 w-full">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-6">
+    <div class="flex justify-between items-center h-16">
+        <div class="flex items-center space-x-2">
+            <img src="{{asset('side_project_logo.png')}}" alt="logo" class="h-8 w-8"/>
+            <span class="font-bold text-lg">Side Project Tracker</span>
+        </div>
+    </div>
+</div>
+</header>
