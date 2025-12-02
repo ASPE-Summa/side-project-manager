7c161bd9d8a74fef3217d63c5c1b3e9dcb9c45af
Peter Askey <aspe@summacollege.nl>
2025-12-02 12:48:59 +0100

    replaced started with completed status and made in progress blue

--- app/Enum/ProjectStatus.php
+++ app/Enum/ProjectStatus.php
@@ -4,9 +4,9 @@ namespace App\Enum;
 enum ProjectStatus: string
 {
     case DRAFT = 'DRAFT';
-    case STARTED = 'STARTED';
     case IN_PROGRESS = 'IN_PROGRESS';
     case PLANNED = 'PLANNED';
     case ON_HOLD = 'ON_HOLD';
     case DROPPED = 'DROPPED';
+    case COMPLETED = 'COMPLETED';
 }
--- resources/views/components/status-badge.blade.php
+++ resources/views/components/status-badge.blade.php
@@ -8,14 +8,9 @@ use App\Enum\ProjectStatus;
         class="inline-flex items-center rounded-md bg-dark-dim px-2 py-1 text-xs font-medium text-grey-0 inset-ring inset-ring-gray-400/20">
         DRAFT
     </span>
-@elseif($status == ProjectStatus::STARTED)
-    <span
-        class="inline-flex items-center rounded-md bg-dark-blue px-2 py-1 text-xs font-medium text-primary inset-ring inset-ring-blue-400/30">
-        STARTED
-    </span>
 @elseif($status == ProjectStatus::IN_PROGRESS)
     <span
-        class="inline-flex items-center rounded-md bg-dark-green px-2 py-1 text-xs font-medium text-success inset-ring inset-ring-green-500/20">
+        class="inline-flex items-center rounded-md bg-dark-blue px-2 py-1 text-xs font-medium text-blue inset-ring inset-ring-green-500/20">
         IN_PROGRESS
     </span>
 @elseif($status == ProjectStatus::DROPPED)
@@ -32,5 +27,10 @@ use App\Enum\ProjectStatus;
     <span
         class="inline-flex items-center rounded-md bg-dark-yellow px-2 py-1 text-xs font-medium text-warning inset-ring inset-ring-yellow-400/20">
         ON_HOLD
+    </span>
+@elseif($status == ProjectStatus::COMPLETED)
+    <span
+        class="inline-flex items-center rounded-md bg-dark-green px-2 py-1 text-xs font-medium text-success inset-ring inset-ring-yellow-400/20">
+        COMPLETED
     </span>
 @endif
